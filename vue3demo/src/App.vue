077faee9c723353896708317c55aedde6210a36f
steven <zou995201314@163.com>
2021-07-12 23:58:04 +0800

    改革开放Vue3demo

--- vue3demo/src/App.vue
+++ vue3demo/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  rou
 </template>
-
+  
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
---
+++ vue3demo/src/views/demo.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container" @scroll="onScroll" ref="container">
+    <div class="panel" :style="{paddingTop : paddingTop + 'px'}">
+      <div class="item" v-for="item in List" :key="item">
+        {{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref,reactive,computed,onMounted } from "vue";
+export default {
+  setup() {
+    let container = ref(null), panel = ref(null);
+    let buffTop = 10 ,buffBottom =10 ,count = 0;
+    let raw = Array(100000).fill(0).map((i,v)=>`item-${i}`) ;
+    let start  = ref(0), end = ref(1);
+    let itemHeight = 1;
+    let paddingTop = ref(0);
+    let List = computed(()=> raw.slice(start.value, end.value) );
+
+    onMounted(()=>{
+      itemHeight = panel.value.firstElementChild.offsetHeight
+      panel.value.style.height = raw.length*itemHeight + 'px'
+      count = Math.floor(container.value.offsetHeight/itemHeight)
+      end.value =count + buffBottom
+    });
+    let timer = null
+    const onScroll = function(e) {
+      if(timer) clearTimeout(timer)
+      timer = setTimeout(()=>{
+        let startValue = Math.floor(e.target.scrolTop / itemHeight)
+        let buff = startValue > buffTop? buffTop:startValue
+        start.value = startValue - buff;
+        end.value = startValue + count + buffBottom
+        paddingTop.value = start.value*itemHeight
+      },200)
+    }
+    return {
+      List , paddingTop,container,panel,onScroll
+    }
+
+  },
+  // name: "feedback",
+  // data() {
+  //   return {
+  //     list:[]
+  //   };
+  // },
+  // computed: {
+    
+  // },
+  // mounted() {
+
+  // },
+  // methods: {
+
+  // }
+};
+</script>
+
+<style lang="less" scoped>
+  *{
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .container {
+    height: 100vh;
+    overflow: scroll;
+  }
+
+  .panel {
+    border: 1px solid red;
+  }
+
+  .item {
+    border: 1px solid #eee;
+    padding: 6px 10px;
+    cursor: pointer;
+  }
+</style>
