9cc849f913672384a838f2e15565b3877556c82e
邹鹏steven <401771873@qq.com>
2021-12-13 11:40:29 +0800

    十一五计划 && buildingData

---
+++ threejs/skyWindow/js/Data/BuildingData.js
@@ -0,0 +1,20 @@
+let buildingObjects = {  // 建筑的类
+    objects:[   // 初始化一个 通用模板  例如 地板
+        {
+            objectName: 'floor',
+            objectType: 'cube',
+            length :  3400,
+            width: 1200,
+            height:1,
+            position:{
+                x:0,
+                y:0,
+                z:0
+            },
+            style:{
+                color: 0X5F7480,
+                opacity: 1
+            }
+        }
+    ]
+}
